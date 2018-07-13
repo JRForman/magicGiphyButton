c003bf6ea96b493c348389027590b2fdef689b9d
Jason Forman <20210580+JRForman@users.noreply.github.com>
2018-07-12 19:40:25 -0700

    Initial files

---
+++ assets/app.js
@@ -0,0 +1,64 @@
+var magicMarvel = ["Captain America", "Thor", "Iron Man", "Black Widow", "Wolverine", "Hulk", "Deadpool", "Black Panther", "Star Lord"];
+
+
+for (x in magicMarvel) {
+    buttonGenerator(magicMarvel[x]);
+
+};
+
+function buttonGenerator(queryWord) {
+
+    var newBtn = $("<button>").addClass("magicButton").text(queryWord).attr("id", queryWord);
+
+
+    $("#buttonContainer").append(newBtn);
+}
+
+function addButton(){
+    
+}
+
+$("#buttonContainer").on("click", function (e) {
+    if (event.target != this) {
+        var person = $(e.target).attr("id");
+        console.log(person);
+
+
+        var queryURL = "https://api.giphy.com/v1/gifs/search?q=" +
+            person + "&api_key=g63epu2bWdVI23mQ1kKiB58btNkpvA3Z&limit=10";
+
+        // Performing our AJAX GET request
+        $.ajax({
+            url: queryURL,
+            method: "GET"
+        })
+            .then(function (response) {
+                var results = response.data;
+                var imgCon = $("#imagesContainer");
+                for (var i = 0; i < results.length; i++) {
+                    if (results[i].rating !== "r" && results[i].rating !== "pg-13") {
+                        var resultImage = $("<img>");
+                        resultImage.addClass("gif");
+                        resultImage.attr("src", results[i].images.fixed_height_still.url);
+                        resultImage.attr("still", results[i].images.fixed_height_still.url);
+                        resultImage.attr("moving", results[i].images.fixed_height.url);
+                        resultImage.attr("state", "still");
+
+                        imgCon.prepend(resultImage);
+                    }
+                }
+            })
+    }
+});
+
+$("#imagesContainer").on("click", function(e){
+    if ((e.target != this)&&(e.target.class="gif")) {
+        if($(e.target).attr("state")=="still"){
+            $(e.target).attr("src",($(e.target).attr("moving")))
+            $(e.target).attr("state","moving")
+        }else{
+            $(e.target).attr("src",($(e.target).attr("still")))
+            $(e.target).attr("state","still")
+        }
+    }
+});
