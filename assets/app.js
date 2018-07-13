67249998fdf5fc51ea05fefe00976b784b4d9118
Jason Forman <20210580+JRForman@users.noreply.github.com>
2018-07-12 20:32:35 -0700

    Styling

--- assets/app.js
+++ assets/app.js
@@ -1,4 +1,4 @@
-var magicMarvel = ["Captain America", "Thor", "Iron Man", "Black Widow", "Wolverine", "Hulk", "Deadpool", "Black Panther", "Star Lord"];
+var magicMarvel = ["Captain America", "Thor", "Iron Man", "Black Widow", "Wolverine", "Hulk", "Deadpool", "Black Panther", "Star Lord", "Gamora", "Nebula"];
 
 
 for (x in magicMarvel) {
@@ -14,20 +14,20 @@ function buttonGenerator(queryWord) {
     $("#buttonContainer").append(newBtn);
 }
 
-function addButton(){
-    
-}
+
+$("#submitButton").on("click", function () {
+    if(($("#addButtonQuery").val())!=""){
+    console.log($("#addButtonQuery").val());
+    buttonGenerator($("#addButtonQuery").val())
+    }
+});
 
 $("#buttonContainer").on("click", function (e) {
     if (event.target != this) {
         var person = $(e.target).attr("id");
-        console.log(person);
-
 
         var queryURL = "https://api.giphy.com/v1/gifs/search?q=" +
-            person + "&api_key=g63epu2bWdVI23mQ1kKiB58btNkpvA3Z&limit=10";
-
-        // Performing our AJAX GET request
+            person + " Marvel"+ "&api_key=g63epu2bWdVI23mQ1kKiB58btNkpvA3Z&limit=10";
         $.ajax({
             url: queryURL,
             method: "GET"
@@ -51,14 +51,14 @@ $("#buttonContainer").on("click", function (e) {
     }
 });
 
-$("#imagesContainer").on("click", function(e){
-    if ((e.target != this)&&(e.target.class="gif")) {
-        if($(e.target).attr("state")=="still"){
-            $(e.target).attr("src",($(e.target).attr("moving")))
-            $(e.target).attr("state","moving")
-        }else{
-            $(e.target).attr("src",($(e.target).attr("still")))
-            $(e.target).attr("state","still")
+$("#imagesContainer").on("click", function (e) {
+    if ((e.target != this) && (e.target.class = "gif")) {
+        if ($(e.target).attr("state") == "still") {
+            $(e.target).attr("src", ($(e.target).attr("moving")))
+            $(e.target).attr("state", "moving")
+        } else {
+            $(e.target).attr("src", ($(e.target).attr("still")))
+            $(e.target).attr("state", "still")
         }
     }
 });
